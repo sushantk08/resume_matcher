862ad3119953d24b27c44f3d93e2446ba9d9cc5c
Sushant Kulkarni <kulkarnisushant87@gmail.com>
2023-10-18 18:55:00 +0530

    fix(ui): properly expose render_batch_match_view in ui package

--- resume_matcher/ui/__init__.py
+++ resume_matcher/ui/__init__.py
@@ -2,11 +2,23 @@
 Streamlit UI module.
 """
 
-from resume_matcher.ui.components import render_sidebar_controls, render_document_input
-from resume_matcher.ui.views import render_single_match_view
+from resume_matcher.ui.components import (
+    render_sidebar_controls,
+    render_document_input,
+    render_overall_score_card,
+    render_skill_badges,
+    render_keyword_table,
+    render_tailoring_tab,
+)
+from resume_matcher.ui.views import render_single_match_view, render_batch_match_view
 
 __all__ = [
     "render_sidebar_controls",
     "render_document_input",
+    "render_overall_score_card",
+    "render_skill_badges",
+    "render_keyword_table",
+    "render_tailoring_tab",
     "render_single_match_view",
+    "render_batch_match_view",
 ]
--- resume_matcher/ui/views.py
+++ resume_matcher/ui/views.py
@@ -127,7 +127,6 @@ def render_single_match_view(config: dict):
                     st.markdown(f"**Matched Resume Experience:**\n* {item['matched_resume_experience']}")
 
         with tab_tailor:
-            # Resolve current text for the sandbox
             current_resume = st.session_state.get("last_resume_input", "")
             if not isinstance(current_resume, str):
                 current_resume = matcher._resolve_text(current_resume, "resume.txt")
@@ -143,3 +142,86 @@ def render_single_match_view(config: dict):
                 current_resume_text=current_resume,
                 jd_text=current_jd,
             )
+
+
+def render_batch_match_view(config: dict):
+    """Render multi-candidate batch triage interface."""
+    st.subheader("👥 Batch Candidate Ranking & Triage")
+    st.caption("Upload multiple candidate resumes at once to automatically rank them against a target job description.")
+
+    col1, col2 = st.columns([1, 1])
+
+    with col1:
+        uploaded_resumes = st.file_uploader(
+            "Upload Multiple Candidate Resumes (.pdf, .docx, .txt)",
+            type=["pdf", "docx", "txt", "md"],
+            accept_multiple_files=True,
+            key="batch_resumes",
+        )
+        if uploaded_resumes:
+            st.success(f"Loaded {len(uploaded_resumes)} candidate resumes.")
+
+    with col2:
+        jd_input, jd_name = render_document_input(
+            title="Target Job Description",
+            key_prefix="batch_jd",
+        )
+
+    st.markdown("<br>", unsafe_allow_html=True)
+    triage_btn = st.button("⚡ Rank All Candidates", type="primary", use_container_width=True)
+
+    if triage_btn:
+        if not uploaded_resumes:
+            st.error("Please upload at least one candidate resume.")
+            return
+        if not jd_input:
+            st.error("Please provide a target Job Description.")
+            return
+
+        with st.spinner(f"Evaluating {len(uploaded_resumes)} candidates across models..."):
+            from resume_matcher.engine.batch_matcher import BatchMatcher
+            matcher = get_cached_matcher()
+            batch_matcher = BatchMatcher(matcher)
+
+            filenames = [f.name for f in uploaded_resumes]
+            leaderboard = batch_matcher.rank_candidates(
+                resumes=uploaded_resumes,
+                filenames=filenames,
+                jd_input=jd_input,
+                jd_filename=jd_name,
+                weights=config["weights"],
+            )
+
+            st.session_state["batch_leaderboard"] = leaderboard
+            st.session_state["has_batch_analyzed"] = True
+
+    # Render Leaderboard
+    if st.session_state.get("has_batch_analyzed", False) and "batch_leaderboard" in st.session_state:
+        from resume_matcher.engine.batch_matcher import BatchMatcher
+        leaderboard = st.session_state["batch_leaderboard"]
+
+        st.markdown("<hr style='margin: 32px 0;'>", unsafe_allow_html=True)
+        st.subheader("🏆 Candidate Fit Leaderboard")
+
+        df = BatchMatcher.to_dataframe(leaderboard)
+        st.dataframe(df, use_container_width=True, hide_index=True)
+
+        # Candidate Deep-Dive Selector
+        st.markdown("#### 🔍 Candidate Deep-Dive")
+        candidate_names = [f"#{c['rank']} - {c['candidate']} ({c['overall_percentage']}%)" for c in leaderboard]
+        selected_cand_label = st.selectbox("Select candidate to inspect detailed breakdown:", candidate_names)
+
+        if selected_cand_label:
+            selected_idx = candidate_names.index(selected_cand_label)
+            cand_data = leaderboard[selected_idx]
+            cand_result = cand_data["full_result"]
+
+            render_overall_score_card(cand_result["overall_fit"])
+
+            c_left, c_right = st.columns(2)
+            with c_left:
+                st.markdown(f"**✅ Matched Skills ({len(cand_data['matched_skills'])})**")
+                render_skill_badges(cand_data["matched_skills"], badge_type="matched")
+            with c_right:
+                st.markdown(f"**⚠️ Missing Skills ({len(cand_data['missing_skills'])})**")
+                render_skill_badges(cand_data["missing_skills"], badge_type="missing")
